7dc2e7d50267b0ff108171909aefa4edec7988ac
Vedran Omeragic <77688940+vedran-omeragic@users.noreply.github.com>
2024-03-08 14:26:10 +0100

    Major refactor of the modal logic and updatse to control/subcontrol ownership modal functionality

--- app/static/js/utils.js
+++ app/static/js/utils.js
@@ -1,7 +1,20 @@
 function disableButton(identifier) {
-    $(identifier).prop("disabled", true);
+  $(identifier).prop("disabled", true);
 }
 
 function enableButton(identifier) {
-    $(identifier).prop("disabled", false);
+  $(identifier).prop("disabled", false);
+}
+
+function isValidNumber(value) {
+  return (
+    value !== null &&
+    value !== undefined &&
+    value !== "" &&
+    !isNaN(Number(value))
+  );
+}
+
+function getNumberOrNull(value) {
+  return isValidNumber(value) ? parseInt(value, 10) : null;
 }
